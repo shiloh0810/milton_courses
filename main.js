1d53a95cb33c56a5623dee345dbb696dcc3a3761
Diego Domenig <diego_domenig20@milton.edu>
2019-02-23 16:14:31 -0500

    Merge branch 'master' of https://github.com/shiloh0810/milton_courses

--- main.js
+++ main.js
@@ -6,23 +6,9 @@ var config = {
   storageBucket: "miltoncourses.appspot.com",
   messagingSenderId: "763338172558"
 };
+
 firebase.initializeApp(config);
-
 const db = firebase.database()
-
-function deleteCourse(subject, course){
-  const ref = db.ref('subject/'+subject)
-  const updates = {}
-  updates['/' + course] = null;
-  ref.update(updates)
-}
-
-function deletecourse(course){
-  const ref = db.ref('subjects')
-  const updates = {}
-  updates['/' + course] = null;
-  ref.update(updates)
-}
 
 function addSubjectCourse(data){
   const ref = db.ref('subjects/'+data.subject)
@@ -37,37 +23,91 @@ function addSubjectCourse(data){
     }
   })
 }
+
 addSubjectCourse({subject:'math', course:'precalc', id:'MPR3'});
-addSubjectCourse({subject:'math', course:'calc', id:'CLC'});
+addSubjectCourse({subject:'math', course:'calc', id:'CLC2'});
+addSubjectCourse({subject:'math', course:'group', id:'asdf'});
+addSubjectCourse({subject:'english', course:'performing', id:'adfad'});
+addSubjectCourse({subject:'english', course:'reading consciousness', id:'addf'});
+addSubjectCourse({subject:'english', course:'human condition', id:'asadfdf'});
+addSubjectCourse({subject:'science', course:'physics', id:'MPR3'});
+addSubjectCourse({subject:'science', course:'biology', id:'CLC2'});
+addSubjectCourse({subject:'science', course:'chemistry', id:'asdf'});
+addSubjectCourse({subject:'history', course:'ancient civilizations', id:'MPR3'});
+addSubjectCourse({subject:'history', course:'us history', id:'CLC2'});
+addSubjectCourse({subject:'history', course:'us in the modern world', id:'asdf'});
+addSubjectCourse({subject:'arts', course:'drawing', id:'MPR3'});
+addSubjectCourse({subject:'arts', course:'sculpture', id:'CLC2'});
+addSubjectCourse({subject:'arts', course:'photography', id:'asdf'});
 
 
-
-
-
-
-
-
-
-
-function sayHello() {
-  const ref = db.ref('courses/math')
-  ref.set({rating:5, teacher:'emmott', credits:1})
-
-}
-function getHello(){
-  const ref = db.ref('courses/us-history')
-  ref.once('value').then(function(snapshot){
-    console.log(snapshot.val())
-  })
-}
-function addsubject(courses){
-  const ref = db.ref("subjects/"+courses.subject)
+function addCourse(data){
+  const ref = db.ref('courses')
   ref.once('value').then(function(snapshot){
     if(snapshot.val() == null){
-      ref.child(courses.id).set({id:courses.rating, counter:1})
+      ref.child(data.course).child('info').set({rating:data.rating, teacher:data.teacher, credit:data.credit, info:data.info})
     }
     else{
-      const currentcounter = snapshot.val().counter
+      const updates = {}
+      updates['/' + data.course] = {rating:data.rating, teacher:data.teacher, credit:data.credit, infos:data.info}
+      ref.update(updates)
     }
   })
+}
+
+addCourse({course:'Algebra 1 with Geometry', rating:null, teacher:'N/A', credit:1, info:'-'})
+addCourse({course:'Geometry', rating:null, teacher:'N/A', credit:1, info:'-'})
+addCourse({course:'Algebra 2', rating:null, teacher:'N/A', credit:1, info:'-'})
+
+
+addCourse({course:'precalc', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'calc', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'group', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'performing', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'reading consciousness', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'human condition', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'physics', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'biology', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'chemistry', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'ancient civilizations', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'us history', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'us in the modern world', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'drawing', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'sculpture', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+addCourse({course:'photography', rating:5, teacher:'precalc', credit:1, info:'testinfo'})
+
+
+function addReview(data){
+  const ref = db.ref('courses/'+data.course+'/reviews')
+  ref.once('value').then(function(snapshot){
+    if(snapshot.val() == null){
+      console.log('hello')
+      ref.child('0').set(data.review)
+    }
+    else{
+      ref.child(snapshot.val().length).set(data.review)
+    }
+  })
+}
+addReview({course:'human condition', review:'very good'})
+
+//add callback here
+function getCourses(){
+  const ref = db.ref('subjects')
+  ref.once('value').then(function(snapshot){
+    for (var key in snapshot.val()) {
+      console.log(snapshot.val()[key])
+    }
+    //Scallback(snapshot.val())
+  })
+}
+getCourses();
+function updateRating(rating, course){
+  const ref = db.ref('courses/'+course)
+  ref.once('value').then(function(snapshot){
+    if(snapshot.val().length == 0){
+      updates['/' + rating] = data.id;
+      ref.update(updates)
+    }
+    })
 }
